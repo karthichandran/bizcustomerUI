2463f57bd820950bac8d248af660713e6cda3eb7
karthi <karthikbalaj.c@gmail.com>
2023-04-18 23:32:55 +0530

    biz customerUI

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  
+  baseApiUrl: 'http://leansyshost-002-site4.atempurl.com',
+  serverUrl: 'http://leansyshost-002-site4.atempurl.com',
+  oauth: {
+    enabled: false,  // For connecting to Mifos X using OAuth2 Authentication change the value to true
+    serverUrl: ''
+  }
+};
